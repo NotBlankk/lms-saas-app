e7cca83deaf67fc3276cf8e1a59248c314c2c492
Ankush Panwar <109392872+NotBlankk@users.noreply.github.com>
2025-06-18 00:12:11 +0530

    Home Page Part1

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,48 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Home Page</h1>
-      <Button>Lol</Button>
-    </div>
+    <main>
+      <h1 className='text-2xl underline'>Popular Companions</h1>
+
+      <section className='home-section'>
+        <CompanionCard
+            id="123"
+            name="Neura the Brainy Explorer"
+            tppic="Neural Network of the Brain"
+            subject="science"
+            duration={45}
+            color="#ffda6e"
+          
+        />
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          tppic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+          
+        />
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          tppic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+          
+        />
+      </section>
+      <section className='home-section'>
+        <CompanionsList/>
+        <CTA/>
+      </section>
+    </main>
     
   )
 }
