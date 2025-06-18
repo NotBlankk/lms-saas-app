b8c973ddcf9e61aaac632cebf3ffba8704ab3e68
Ankush Panwar <109392872+NotBlankk@users.noreply.github.com>
2025-06-19 01:01:58 +0530

    clerk integration

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 
-// import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import NavItems from "@/components/NavItems";
 
 const Navbar = () => {
@@ -20,14 +20,14 @@ const Navbar = () => {
             
             <div className="flex items-center gap-8">
                 <NavItems />
-                {/* <SignedOut>
+                <SignedOut>
                     <SignInButton>
                         <button className="btn-signin">Sign In</button>
                     </SignInButton>
                 </SignedOut>
                 <SignedIn>
                     <UserButton />
-                </SignedIn> */}
+                </SignedIn>
             </div>
         </nav>
     )
